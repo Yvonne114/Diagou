20988f603af01ec187839649ce827cee16a8d5ac
Yvonne Chien <morneca60523@gmail.com>
2026-04-04 23:03:35 +0800

    Initial commit: add frontend and backend

---
+++ diagou-backend/src/main/java/com/diagou/backend/DiagouBackendApplication.java
@@ -0,0 +1,13 @@
+package com.diagou.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiagouBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiagouBackendApplication.class, args);
+	}
+
+}
